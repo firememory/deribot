78d944fad5049e96b34d74e9f837e81c11962a86
renaud <renaud@debian>
2017-01-14 12:29:13 +0800

    [API] Added a loop mode with callbacks [Recorder] Fixed the periodic disconnections

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ import base64
 import hmac
 import os
 import ssl
+import threading
+from time import sleep
 
 ### TODO ###
 # options, --test, --ping etc...
@@ -15,33 +17,71 @@ import ssl
 # factoriser les options send
 # getId() with a reset to 0
 
+KEEP_ALIVE_TIMEOUT = 10
+
 class API(object):
 
-    def __init__(self, keyFile):
-        websocket.enableTrace(True)
-        self.ws_ = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
-#        self.ws_.connect("wss://test.deribit.com/ws/api/v1/")
-        self.ws_.connect("wss://www.deribit.com/ws/api/v1/")
+    def __init__(self, keyFile, callbackObject=None, verbose=False):
+        print "Connecting"
+        websocket.enableTrace(verbose)
+        self.ws_ = websocket.WebSocketApp("wss://www.deribit.com/ws/api/v1/",
+                                          on_open = self.on_open,
+                                          on_message = self.on_message,
+                                          on_error = self.on_error,
+                                          on_close = self.on_close)
         self.load_key(keyFile)
         self.id_ = 0
+        self.verbose = verbose
+
+        if callbackObject != None: # loop mode
+            print "Launching main loop"
+            self.callbackObject = callbackObject
+            self.isRunning = True
+            self.ws_.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        else:
+            self.ws_ = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+            self.ws_.connect("wss://www.deribit.com/ws/api/v1/")
+
+    def on_open(self, ws):
+        print "Starting keep alive thread"
+        self.keepAliveThread = threading.Thread(target=self.keepAlive)
+        self.keepAliveThread.start()
+
+        if self.callbackObject:
+            self.callbackObject.onOpen(self)
+
+    def on_message(self, ws, message):
+        message = json.loads(message)
+        if self.verbose:
+            print message
+        if "result" in message.keys() and message["result"] == "pong":
+            pongTime = time.time()
+            print ("PONG ! It took %.1fms rtt" % ((pongTime - self.pingTime) * 1000))
+        else:
+            if self.callbackObject:
+                self.callbackObject.onMessage(message)
+
+
+    def on_close(self, ws):
+        print "on_close called"
+        self.isRunning = False
+        self.keepAliveThread.join()
+
+    def on_error(self, ws, error):
+        print "ERROR: %s" % error
+
+    def keepAlive(self):
+        while self.isRunning:
+            self.ping()
+            for i in range(0, KEEP_ALIVE_TIMEOUT):
+                if not self.isRunning:
+                    break
+                sleep(1)
 
     def ping(self):
-        print ("==========")
-        print ("= PING ? =")
-        print ("==========")
-        t1 = time.time()
+        print ("PING ?")
+        self.pingTime = time.time()
         self.send_public("ping")
-        res = api.receive()
-        t2 = time.time()
-        if "result" in res and res["result"] == "pong":
-            print ("==========")
-            print ("= PONG ! =")
-            print ("==========")
-
-            print ("It took %.1fms rtt" % ((t2 - t1) * 1000))
-        else:
-            print ("ERROR:")
-            print (res)
 
     def load_key(self, path):
         f = open(path, "r")
@@ -53,8 +93,10 @@ class API(object):
         req = { "id"            : self.id_,
                 "action"        : "/api/v1/public/" + action,
                 "arguments"     : arguments,
-            }
+        }
         req = json.dumps(req)
+        if self.verbose:
+            print req
         self.ws_.send(req)
         self.id_ += 1
 
@@ -63,7 +105,6 @@ class API(object):
         sig = "_=%s&_ackey=%s&_acsec=%s&_action=%s" % (nonce, self.key_, self.secret_, action)
         for key in sorted(arguments.keys()):
             sig += "&%s=%s" % (key, "".join(arguments[key]))
-        print sig
         h = hashlib.new("sha256", sig)
         sig = h.digest()
         sig = base64.b64encode(sig)
@@ -77,7 +118,8 @@ class API(object):
                 "sig"           : signature,
             }
         req = json.dumps(req, ensure_ascii=False)
-        print req
+        if self.verbose:
+            print req
         self.ws_.send(req)
         self.id_ += 1
 
--- recorder.py
+++ recorder.py
@@ -1,60 +1,70 @@
 from api import API
 from optparse import OptionParser
-from time import sleep
+from time import sleep, time
+#from sys.stdout import flush
+import sys
 
 class Recorder(object):
-    def __init__(self):
-        self.api = API("key.txt")
-        self.run = True
-        self.book = {}
+    def __init__(self, verbose=False):
+        self.verbose = verbose
 
     def listAll(self):
-        self.api.send_public("getinstruments")
-        res = self.api.receive()
+        api = API("key.txt")
+        api.send_public("getinstruments")
+        res = api.receive()
         for instr in res["result"]:
             print "%s\t%s" % (instr["instrumentName"], instr["kind"])
 
     def writeUpdate(self, ts, update):
+        print "update=%s"%update
         instr = update["result"]["instrument"]
         if instr not in self.book.keys():
             self.book[instr] = {}
+        recvTime = time() * 100
         for bidUpdate in update["result"]["bids"]:
             prcUpdate = bidUpdate["price"]
             qtyUpdate = bidUpdate["quantity"]
             if prcUpdate not in self.book[instr].keys() or self.book[instr][prcUpdate] != qtyUpdate:
                 self.book[instr][prcUpdate] = qtyUpdate
 #                print instr, "B", prcUpdate, qtyUpdate
-                self.fout.write("%s;%s;B;%s;%s\n" % (ts, instr, prcUpdate, qtyUpdate))
+                self.fout.write("%s;%s;%s;B;%s;%s\n" % (ts, recvTime, instr, prcUpdate, qtyUpdate))
         for askUpdate in update["result"]["asks"]:
             prcUpdate = askUpdate["price"]
             qtyUpdate = askUpdate["quantity"]
             if prcUpdate not in self.book[instr].keys() or self.book[instr][prcUpdate] != qtyUpdate:
                 self.book[instr][prcUpdate] = qtyUpdate
 #                print instr, "S", prcUpdate, qtyUpdate
-                self.fout.write("%s;%s;S;%s;%s\n" % (ts, instr, prcUpdate, qtyUpdate))
+                self.fout.write("%s;%s;%s;S;%s;%s\n" % (ts, recvTime, instr, prcUpdate, qtyUpdate))
+        self.fout.flush()
+
+    def onOpen(self, api):
+        print "Subscribing to updates of %s" % self.instr
+        api.send_private("subscribe", {"instrument": [self.instr], "event": ["order_book", "trade", "user_order"]})
+
+    def onMessage(self, message):
+        if "notifications" in message.keys():
+            for update in message["notifications"]:
+                self.writeUpdate(message["msOut"], update)
+        else:
+            print "ERROR: not a notification: %s" % message
 
     def start(self, instr):
-        print "Starting to record %s" % instr
-        self.fout = open("/home/renaud/quotes/deribit.csv", "w")
-        self.api.send_private("subscribe", {"instrument": [instr], "event": ["order_book", "trade", "user_order"]})
-#        print self.api.receive()
-        while self.run:
-            res = self.api.receive()
-            if "notifications" in res.keys():
-                for update in res["notifications"]:
-                    self.writeUpdate(res["msOut"], update)
-        self.api.close()
-        self.fout.close()
+        self.fout = open("/home/renaud/quotes/deribit.csv", "w+")
+        self.book = {}
+        self.instr = instr
+        self.api = API("key.txt", self, self.verbose)
+
 
 if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-i", "--instr",   action="store",      help="Instrument to record")
     parser.add_option("-l", "--listall", action="store_true", help="List all instruments")
+    parser.add_option("-v", "--verbose", action="store_true", help="Verbose mode")
     (options, args) = parser.parse_args()
     if not options.instr and not options.listall:
         parser.error("--instr or --listall needed")
 
-    r = Recorder()
+    r = Recorder(options.verbose)
 
     if options.listall:
         r.listAll()
