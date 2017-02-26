a36703a21ce55fd57aa2cb5d7c77dbe95ee0c971
renaud <renaud@debian>
2017-02-26 09:35:47 +0800

    [Recorder] Fixed the Makefile + started to implement the authentication + added the fix conf to the repo

--- recorder.h
+++ recorder.h
@@ -1,6 +1,8 @@
 #ifndef _RECORDER_H_
  #define _RECORDER_H_
 
+#include <atomic>
+
 #include "quickfix/FileStore.h"
 #include "quickfix/FileLog.h"
 #include "quickfix/SocketAcceptor.h"
@@ -21,6 +23,11 @@ public:
     throw (FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::RejectLogon);
   void fromApp(const FIX::Message& msg, const FIX::SessionID& id)
     throw (FIX::FieldNotFound, FIX::IncorrectDataFormat, FIX::IncorrectTagValue, FIX::UnsupportedMessageType);
+
+//   std::atomic<bool> isRunning() { return isRunning_; };
+
+// private:
+//   std::atomic<bool> isRunning_;
 };
 
 #endif // _RECORDER_H_
